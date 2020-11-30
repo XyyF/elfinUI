0a5799b973975a91ac4fd859e6f32268b53ce6be
rengarxiao <rengarxiao@tencent.com>
2020-11-30 15:03:18 +0800

    feat: add eslint && commit-msg

---
+++ .elfin.external.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {
+        sshAddress: 'git@github.com:XyyF/elfin-eslint.git',                   /* git ssh地址 */
+        branch: 'main',              /* clone的分支，默认master */
+        skipInstall: false,             /* 是否跳过install依赖，默认为false */
+    },
+]
---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['../elfin-eslint/.eslintrc.js'],
+};
